bfc84e2ecb147eaad1c40bdc1bbbd83ee66d427a
ParkYoungWoong <thesecon@gmail.com>
2026-09-08 09:49:39 +0900

    26-09-08 09:49

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './main.css'
-// import App from './App'
 import Router from './routes'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
-  <>
+  <QueryClientProvider client={queryClient}>
     <Router />
-  </>
+  </QueryClientProvider>
 )
--- src/routes/pages/MovieDetails.tsx
+++ src/routes/pages/MovieDetails.tsx
@@ -1,24 +1,25 @@
-import { useState, useEffect } from 'react'
 import { useParams, useSearchParams } from 'react-router'
 import type { Movie } from '@/types/movie'
+import { useQuery } from '@tanstack/react-query'
 
 // http://localhost:5173/movies/tt12345678?plot=full
 export default function MovieDetails() {
   const { movieId } = useParams()
   const [searchParams] = useSearchParams()
   const plot = searchParams.get('plot') || 'short'
-  const [movie, setMovie] = useState<Movie | null>(null)
 
-  useEffect(() => {
-    async function fetchMovie() {
+  // const 반환 = useQuery(옵션)
+  const { data: movie } = useQuery<Movie>({
+    queryKey: ['movie', movieId],
+    queryFn: async () => {
       const res = await fetch(
         `https://omdbapi.com?apikey=9d38c929&i=${movieId}&plot=${plot}`
       )
-      const data = await res.json()
-      setMovie(data)
-    }
-    fetchMovie()
-  }, [])
+      const abc = await res.json()
+      return abc
+    },
+    staleTime: 1000 * 60 * 5
+  })
 
   return (
     <>
